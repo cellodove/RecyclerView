2212942b57fea573e1184f5e9bf1db5f2dc8c464
jusung.kin@skylabs.io <wntjd200@naver.com>
2021-12-07 11:41:13 +0900

    recyclerView add

--- app/src/main/java/com/cellodove/recyclerview/main/MainViewModel.kt
+++ app/src/main/java/com/cellodove/recyclerview/main/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.cellodove.recyclerview.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cellodove.recyclerview.repository.model.ProfileListInfo
 import com.cellodove.recyclerview.util.SingleLiveEvent
 import com.cellodove.recyclerview.util.Timer
 import kotlinx.coroutines.CoroutineScope
@@ -15,22 +16,22 @@ class MainViewModel : ViewModel() {
     private val uiScope = CoroutineScope(Dispatchers.Main + baseJob)
 
     enum class FragmentStep { HOME, PROFILE_DETAIL }
-
-
-
-    private var scanTimer = Timer(30000) {  }
     val liveFragmentStep = SingleLiveEvent<FragmentStep>()
 
-    var responseData = MutableLiveData<Int>()
-    var responseComplete = MutableLiveData<Int>()
-    var insertData = MutableLiveData(false)
-    var liveWifiList = MutableLiveData(arrayListOf<String>())
 
-
+    var gatewayData = MutableLiveData(arrayListOf<ProfileListInfo>())
 
     init {
-        viewModelScope.launch {
-        }
+        var gatewayInfo = arrayListOf<ProfileListInfo>()
+        gatewayInfo.add(ProfileListInfo("1","기계공학부","홍길동","20217724"))
+        gatewayInfo.add(ProfileListInfo("2","컴퓨터공학부","고길동","20217724"))
+        gatewayInfo.add(ProfileListInfo("3","전기공학부","이철수","20217724"))
+        gatewayInfo.add(ProfileListInfo("4","전자공학부","고길순","20217724"))
+        gatewayInfo.add(ProfileListInfo("5","화학공학부","김짱아","20217724"))
+        gatewayInfo.add(ProfileListInfo("6","컴퓨터공학부","신짱구","20217724"))
+        gatewayInfo.add(ProfileListInfo("7","전자공학부","이유리","20217724"))
+        gatewayInfo.add(ProfileListInfo("8","화학공학부","이세아","20217724"))
+        gatewayInfo.add(ProfileListInfo("9","기계공학부","박진아","20217724"))
+        gatewayData.value = gatewayInfo
     }
-
 }
--- app/src/main/java/com/cellodove/recyclerview/main/RecyclerAdapterFragment.kt
+++ app/src/main/java/com/cellodove/recyclerview/main/RecyclerAdapterFragment.kt
@@ -13,7 +13,6 @@ class RecyclerAdapterFragment : Fragment() {
 
     private lateinit var binding : FragmentRecylerAdapterBinding
     private val viewModel : MainViewModel by activityViewModels()
-    private var gatewayData = arrayListOf<ProfileListInfo>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentRecylerAdapterBinding.inflate(inflater,container,false)
@@ -22,28 +21,19 @@ class RecyclerAdapterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.profileDetail.setOnClickListener {
+        binding.profileDetail.setOnClickListener {}
 
-        }
-
-        //gatewayData.clear()
-        gatewayData.add(ProfileListInfo("1","기계공학부","홍길동","20217724"))
-        gatewayData.add(ProfileListInfo("2","컴퓨터공학부","고길동","20217724"))
-        gatewayData.add(ProfileListInfo("3","전기공학부","이철수","20217724"))
-        gatewayData.add(ProfileListInfo("4","전자공학부","고길순","20217724"))
-        gatewayData.add(ProfileListInfo("5","화학공학부","김짱아","20217724"))
-        gatewayData.add(ProfileListInfo("6","컴퓨터공학부","신짱구","20217724"))
-        gatewayData.add(ProfileListInfo("7","전자공학부","이유리","20217724"))
-        gatewayData.add(ProfileListInfo("8","화학공학부","이세아","20217724"))
-        gatewayData.add(ProfileListInfo("9","기계공학부","박진아","20217724"))
-        var recyclerAdapter = RecyclerAdapter(gatewayData)
-        binding.gatewayList.adapter = recyclerAdapter
-        binding.profileDetail.setOnClickListener {
-            viewModel.liveFragmentStep.value = MainViewModel.FragmentStep.PROFILE_DETAIL
-        }
-        recyclerAdapter.setOnItemClickListener(object : RecyclerAdapter.OnItemClickListener{
-            override fun onItemClick(position: Int) {
+        viewModel.gatewayData.observe(viewLifecycleOwner){
+            var recyclerAdapter = RecyclerAdapter(it)
+            binding.gatewayList.adapter = recyclerAdapter
+            binding.profileDetail.setOnClickListener {
+                viewModel.liveFragmentStep.value = MainViewModel.FragmentStep.PROFILE_DETAIL
             }
-        })
+            recyclerAdapter.setOnItemClickListener(object : RecyclerAdapter.OnItemClickListener{
+                override fun onItemClick(position: Int) {
+                }
+            })
+        }
+
     }
 }
