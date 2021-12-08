7afd14908245f1e9d26b02d44f666be65866fa77
wntjd200@naver.com <wntjd200@naver.com>
2021-12-08 23:37:31 +0900

    리스트뷰 추가중....

--- app/src/main/java/com/cellodove/recyclerview/main/ListAdapter.kt
+++ app/src/main/java/com/cellodove/recyclerview/main/ListAdapter.kt
@@ -1,58 +1,40 @@
 package com.cellodove.recyclerview.main
 
-import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.cellodove.recyclerview.databinding.ListAdapterItemBinding
 
-class ListAdapter(var wifiData : ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ProfileDetailAdapter() : ListAdapter<ArrayList<String>, ProfileDetailAdapter.ProfileDataViewHolder>(diffUtil) {
 
-    interface OnItemClickListener{
-        fun onItemClick(position: Int)
-    }
-    private lateinit var onItemClickListener : OnItemClickListener
-
-    fun setOnItemClickListener(listener : OnItemClickListener){
-        this.onItemClickListener = listener
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileDataViewHolder {
+        TODO("Not yet implemented")
     }
 
-    inner class WifiListViewHolder(private val binding: ListAdapterItemBinding, context: Context) : RecyclerView.ViewHolder(binding.root), View.OnClickListener{
-        var context = context
+    override fun onBindViewHolder(holder: ProfileDataViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ProfileDataViewHolder(private val binding: ListAdapterItemBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener{
         var onBindPosition = 0
 
-        fun onBind(wifiData: String, position: Int){
-            binding.userName.text = wifiData
+        fun onBind(profileData: String, position: Int){
+            binding.userName.text = profileData
             onBindPosition = position
             binding.profileImage.setOnClickListener(this)
         }
 
         override fun onClick(view: View?) {
-            if (view?.id == binding.profileImage.id){
-                onItemClickListener.onItemClick(onBindPosition)
-            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return WifiListViewHolder(
-            ListAdapterItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            ), parent.context
-        )
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is WifiListViewHolder){
-            var gatewayListViewHolder = holder as WifiListViewHolder
-            gatewayListViewHolder.onBind(wifiData[position],position)
+    companion object{
+        val diffUtil = object : DiffUtil.ItemCallback<ArrayList<String>>(){
+            override fun areItemsTheSame(oldItem: ArrayList<String>, newItem: ArrayList<String>) = oldItem == newItem
+            override fun areContentsTheSame(oldItem: ArrayList<String>, newItem: ArrayList<String>) = oldItem == newItem
         }
-    }
-    override fun getItemCount(): Int {
-        return wifiData.size
     }
 
 
